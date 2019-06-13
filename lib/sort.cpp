0f11430899ec2f2f212134fee32aaee6724e35aa
DaniilOkulov134421 <dan-okulov2009@yandex.ru>
2019-06-13 20:43:25 +0300

    builded on linux

---
+++ lib/sort.cpp
@@ -0,0 +1,64 @@
+#include "sort.h"
+
+/****RECURSIVE****/
+
+void internal_qsort_recursive(
+	String::iterator left_idx,
+	String::iterator right_idx) {
+
+	if(std::distance(left_idx, right_idx) < 1) return;
+
+	auto
+		start_idx = left_idx,
+		end_idx = right_idx,
+		base_idx = left_idx + std::distance(left_idx, right_idx) / 2;
+
+	while(left_idx < right_idx) {
+		while(*left_idx < *base_idx && left_idx < base_idx) left_idx++;
+		while(*right_idx >= *base_idx && right_idx > base_idx) right_idx--;
+
+		if(left_idx < right_idx) std::iter_swap(left_idx, right_idx);
+		if(left_idx == base_idx) base_idx = right_idx;
+		else if(right_idx == base_idx) base_idx = left_idx;
+	}
+	
+	if(base_idx > start_idx)
+		internal_qsort_recursive(start_idx, base_idx);
+	if(base_idx < end_idx)
+		internal_qsort_recursive(base_idx + 1, end_idx);
+}
+
+void qsort_recursive(String & raw) {
+	internal_qsort_recursive(raw.begin(), raw.end() - 1);
+}
+
+void file_read(const String path_to_file, String & data) {
+	std::ifstream f;
+  f.open(path_to_file, std::ios::in);//точный адрес хранения необходимого файла
+  if (f)
+    while(!f.eof())
+      f>>data;
+  else
+    std::cout <<"Error!\n";
+  f.close();
+}
+
+void file_write(const String path_to_file, String & data) {
+	std::ofstream n;
+  n.open(path_to_file, std::ios::out);;//удаляем данные о предыдущем файле
+  if (!(n.is_open()))
+    std::cout << "File not find\n";
+  else {
+    n<<data;//перезаписываем файл
+    n.close();
+  }
+}
+
+void sort_file(const String path_to_file)
+{
+  String unsorted;
+  file_read(TEST_PATH, unsorted);
+  qsort_recursive(unsorted);
+  file_write(TEST_PATH, unsorted);
+}
+    
---
+++ lib/sort.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+
+template<class Type>
+using Vector = std::vector<Type>;
+using String = std::string;
+
+static const String TEST_PATH = "cpp.txt";
+
+void qsort_recursive(String & raw);
